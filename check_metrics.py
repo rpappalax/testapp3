dc85ba3cf68a289bbc3e2d3873323254d958c63d
Richard Pappalardo <rpappalax@gmail.com>
2023-07-30 12:57:21 +0200

    update check_metrics

--- check_metrics.py
+++ check_metrics.py
@@ -1,10 +1,26 @@
+"""Telemetry Probe Expiry Monitor.
+
+PURPOSE:
+The purpose of this module is to:
+1. Monitor telemetry probe YAML files for expired probes
+2. Send a slack notification for (soon-to-be) expired probes
+
+"""
 import re
 import yaml
+import urllib.request
+import configparser
 
 from date_checker import soon_expiring
 
 
-EXP_WARNING_DAYS = 30
+CONFIG_INI = 'check_metrics.ini'
+WARN_THRESHOLD_DAYS = 30
+expired_already = []
+expiring_soon = []
+
+
+METRICS_FILENAME = 'metrics.yaml'
 
 
 def filestream(filename):
@@ -21,12 +37,40 @@ def is_date_format(date_input):
     if re.match(pattern_str, date_input):
         return True 
     else:
-        return False 
+        return False
+
+
+"""
+def projects(config):
+    # traverse all projects one-by-one
+    print(config.sections())
+    #print(config["focus_ios"]["file"])
+    print(x)
+    for section in config.sections():
+        print(section)
+        
+        for item in config.items(section):
+            print(item)
+"""
+
+
+def project(CONFIG_INI, project):
+    """ Given a project name, download telemetry yaml, return warn window """
+
+    config = configparser.ConfigParser()
+    #c = config.read(CONFIG_INI)
+    config.read('check_metrics.ini')
+
+    # pull data from a single project
+    url = config.get('focus-ios', 'file')
+    warn = config.get(project, 'WARN_THRESHOLD_DAYS')
+
+    # download metrics yaml file
+    urllib.request.urlretrieve(url, METRICS_FILENAME)
+    return warn
 
 
 def print_dict(v, prefix=''):
-    expired_already = []
-    expiring_soon = []
     if isinstance(v, dict):
         for k, v2 in v.items():
             p2 = "{}['{}']".format(prefix, k)
@@ -42,19 +86,33 @@ def print_dict(v, prefix=''):
         tmp = []
         if 'expires' in prefix:
             if is_date_format(result):
-                exp = soon_expiring(result, EXP_WARNING_DAYS)
+                exp = soon_expiring(result, WARN_THRESHOLD_DAYS)
                 if exp:
-                    print(f'EXPIRED IN {EXP_WARNING_DAYS} DAYS!: {prefix}: {result}')
+                    print(f'WARN_THRESHOLD_DAYS: {WARN_THRESHOLD_DAYS}: {prefix}: {result}, {exp}')
                     tmp = [prefix, result]
-                    expiring_soon.append(tmp)
+                    if exp <=0:
+                        expired_already.append(tmp)
+                    elif (exp > 0 and exp <= WARN_THRESHOLD_DAYS):
+                        expiring_soon.append(tmp)
+                    else:
+                        print('not expiring')
+                        
 
-    return expiring_soon
+    return expired_already, expiring_soon
                    
 
 
 if __name__ == '__main__':
 
-    metrics = filestream('metrics.yaml')
-    report = print_dict(metrics)
+    #config_parse(CONFIG_INI)
+    project(CONFIG_INI, 'focus-ios') 
+
+    import sys
+    sys.exit()
+    metrics = filestream(METRICS_FILENAME)
+    already, soon= print_dict(metrics)
     print('======')
-    print(report)
+    print(already)
+    print('======')
+    print(soon)
+
--- date_checker.py
+++ date_checker.py
@@ -1,25 +1,34 @@
 import datetime
 
 
-def soon_expiring(exp_date, exp_warning_days=60):
-    # setup dates
-    expiration_date = datetime.datetime.strptime(exp_date, '%Y-%m-%d')
-    today = datetime.datetime.today()
+def soon_expiring(date_input, warn_threshold_days=60):
+    """ Parse dates into 3 categories:
+    1. Already expired - date_input <= today (i.e. before today)
+    2. Soon expiring - date_input > today but <= the number of upcoming days to watch
+    3. Not expiring - date_input > the number of upcoming days to watch
+    """
 
-    # compute difference
-    ndays = (expiration_date -  today).days
+    # format dates
+    date_input_formatted = datetime.datetime.strptime(date_input, '%Y-%m-%d')
+    today_formatted = datetime.datetime.today()
 
+    # date differential as # of days to warning threshold
+    differential_days = (date_input_formatted -  today_formatted).days
+
+    return differential_days
     # print output
-    #print(f'DAYS TIL EXPIRATION: {ndays}')
-    if ndays <= exp_warning_days:
-        return True 
+    #print(f'DAYS TIL EXPIRATION: {differential_days}')
+    """
+    if differential_days <= warn_threshold_days:
+        return differential_days 
     else:
         return False
+    """
 
 
 if __name__ == '__main__':
 
-    expiration_date = '2023-08-06'
-    result = soon_expiring(expiration_date)
+    date_input_formatted = '2023-08-06'
+    result = soon_expiring(date_input_formatted)
     print(result)
 
